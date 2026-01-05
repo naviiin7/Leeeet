203bcaf85fbdef3eb87b919d908f0244f3d00843
Navin <navin.jeys@gmail.com>
2026-01-05 10:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (51.34%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -1,18 +1,8 @@
-array = [1,3,4,6,7,8,9];
 var searchInsert = function(nums, target) {
-    let arr1 = [];
-    for ( let i=0; i<nums.length; i++){
-        if(nums[i] === target){
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
             return i;
         }
-        else{  
-            nums.push(target);
-            arr1 = nums.sort((a,b) => a - b);
-            var tgt = arr1.indexOf(target)
-;
-            return tgt;
-        }
-
     }
-    
+    return nums.length;
 };
