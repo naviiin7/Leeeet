fd459115a15e75c62f67928c402927da46fe7e29
Navin <navin.jeys@gmail.com>
2026-03-10 10:28:15 +0530

    Time: 1 ms (100.00%) | Memory: 62.3 MB (99.46%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+
+        int left = 1;
+        for(int i = 0; i < n; i++)
+        {
+            res[i] = left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            res[i] *= right;
+            right *= nums[i];
+        }
+
+        return res;
+    }
+}
