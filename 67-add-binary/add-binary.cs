eaf53613cffeec9a48cd9397c6bd3d4efdd78b38
Navin <navin.jeys@gmail.com>
2026-04-07 10:24:27 +0530

    Time: 1 ms (87.51%) | Memory: 41.3 MB (95.28%) - LeetSync

---
+++ 67-add-binary/add-binary.cs
@@ -0,0 +1,30 @@
+public class Solution 
+{
+    public string AddBinary(string a, string b) 
+    {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        var result = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int sum = carry;
+
+            if (i >= 0)
+                sum += a[i--] - '0';
+
+            if (j >= 0)
+                sum += b[j--] - '0';
+
+            result.Append(sum % 2);
+            carry = sum / 2;
+        }
+
+        char[] arr = result.ToString().ToCharArray();
+        Array.Reverse(arr);
+
+        return new string(arr);
+    }
+}
