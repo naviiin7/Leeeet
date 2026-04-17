075c789c59cd93037bfedee836866b277ca2190c
Navin <navin.jeys@gmail.com>
2026-04-17 10:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (93.84%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
@@ -3,19 +3,17 @@ public class Solution
     public int MaxProfit(int[] prices) 
     {
         int profit = 0; 
-        int maxP = 0;
         for(int i=0; i<prices.Length -1; i++)
         {
             if(prices[i] < prices[i+1])
             {
-                profit = prices[i+1] - prices[i];
-                maxP += profit;
+                profit += prices[i+1] - prices[i];
             }
             else
                 continue;
                         
         }
-        return maxP;
+        return profit;
         
     }
 }
